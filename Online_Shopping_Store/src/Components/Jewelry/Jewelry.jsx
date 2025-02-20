0f768fc7725b948e3984786c1c00589ede334761
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-20 15:27:59 +0500

    Add files via upload

---
+++ Online_Shopping_Store/src/Components/Jewelry/Jewelry.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+export default function Jewelry() {
+  return (
+    <>       
+       <div className='grid grid-cols-4 '>
+    <figure className=' h-[31rem] w-[20rem] rounded-xl m-12 mt-24'>
+      <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/d3/38/88/d33888ef84baac80e4a59495139156c4.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 90,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Crystal Bridal Jewelry </h2>
+        </div>
+    </figure>
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+      <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/54/ea/66/54ea66b53382af346dc127e9ed36a5ea.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 100,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Zircon Clear Necklace</h2>
+        </div>
+    </figure>
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+      <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/2c/59/b5/2c59b5b5247af4e9dda1651a5a8b5de3.jpg" alt="" />
+      <div className='transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 100,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Bridal Diamond Jewelry</h2>
+        </div>
+    </figure>
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+      <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/ad/8e/43/ad8e43cbf473872b327130440a338010.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 200,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Crystal Bridal Jewelry </h2>
+        </div>
+    </figure> 
+    <figure className=' h-[32rem] w-[20rem] rounded-xl ml-[3rem] -mt-32'>
+      <img className='p-2 cursor-pointer' src="https://i.pinimg.com/736x/90/01/96/900196f40deb55c38d4c4cf560937e5b.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 300,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Crystal Silver Bridal Necklace</h2>
+        </div>
+    </figure>
+    
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-32'>
+      <img className='p-2 cursor-pointer' src="https://i.pinimg.com/564x/8a/bc/22/8abc22f015521d4527ac17f85eb69a83.jpg" alt="" />
+      <div className='transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 300,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Multi Later Swarovski Set</h2>
+        </div>
+    </figure>
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-32'>
+      <img className='p-2 cursor-pointer' src="https://i.pinimg.com/736x/f6/76/c2/f676c2313a98fa3fa1cf729f1c7bb17e.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 500,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Dream Jewelry Necklace</h2>
+        </div>
+    </figure>
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-32'>
+      <img className='p-2 cursor-pointer' src="https://i.pinimg.com/564x/fa/d6/31/fad63126c078019d4e513b5d96ecead8.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 500,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Handmade Unique Jewelry</h2>
+        </div>
+    </figure>
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-32 '>
+      <img className='p-2 cursor-pointer' src="https://i.pinimg.com/564x/ef/72/7b/ef727b74c0dcd4dc138ad4fabd8c9654.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 400,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Adorable Design Crystal Jewelry</h2>
+        </div>
+    </figure> 
+    <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-32'>
+      <img className='p-2 cursor-pointer' src="https://i.pinimg.com/736x/1a/fa/a8/1afaa8d6720ca82ac96087622015eb4d.jpg" alt="" />
+      <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+          <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 800,000</span>
+         <h2 className='font-semibold text-2xl text-center '>Elegant and Simple Design</h2>
+        </div>
+    </figure>
+    
+   </div></>
+  )
+}
