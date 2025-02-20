e1bacfe7f3db82be5e9f760caab3299759d59719
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-20 15:26:02 +0500

    Add files via upload

---
+++ Online_Shopping_Store/src/Components/NewArrivals/NewArrivals.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function NewArrivals() {
+  return (
+    <>
+     <div className='grid grid-cols-4 gap-4'>
+      <figure className=' h-[31rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/92/27/bc/9227bc1ed11489d9f397d94dd6b4f1b3.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 10,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Blue Velvet </h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/92/99/16/929916788aba3857d29e6bcb6a6ad99e.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 5000</span>
+           <h2 className='font-semibold text-2xl text-center '>Black ghagra </h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/65/fc/ea/65fcea8d33f33985950dbaf4b8da1fe2.jpg" alt="" />
+        <div className='transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 4000</span>
+           <h2 className='font-semibold text-2xl text-center '>Black Maxi </h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/61/dc/fd/61dcfd7890acc0a2189bae0595805b40.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 6000</span>
+           <h2 className='font-semibold text-2xl text-center '>White Maxi </h2>
+          </div>
+      </figure> 
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/f4/bb/ca/f4bbca907aef302aed89f863235f7587.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 50,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Heavy embroidery Red Lehnga</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/68/e6/59/68e6591ec1fecdf6689e6f0b44a75ea0.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 50,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Peach Digital Printed Lehnga</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/1f/26/37/1f263759562369d17072ca49ef367dc0.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 12,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Bohemian Maxi</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/15/83/5f/15835f6d3fa7890b134829ef0e30149f.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 8,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Grey Maxi With Printed Design</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/8b/0d/63/8b0d63d12165be5da8fa766381e12797.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 6,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Navy Blue Winter Karandi</h2>
+          </div>
+      </figure> 
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/b6/d5/36/b6d536632b3d0b6e6134d24e437c75e2.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 7,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Off White Winter Karandi</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/fe/7d/95/fe7d95555145211831a36a693eb7526c.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 5,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Blue Lawn Suit</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/79/56/0a/79560a1448d0055651f60f93cf85ff3b.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 13,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Unstitched Navy Blue Embroidery</h2>
+          </div>
+      </figure>  
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/56/d1/83/56d18399f8264d1362bac8160b98fa5e.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 90,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Aesthetic White and Peach Set</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/1d/c4/3d/1dc43d933aec91b2e77001e1a7602a40.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 100,000</span>
+           <h2 className='font-semibold text-2xl text-center '> Rings Aesthetic</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/f8/06/8a/f8068a4918209c754930f2ab7d2233aa.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 1,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Jhumka</h2>
+          </div>
+      </figure> 
+       <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/98/33/ac/9833acf9db0c6cade8c69597494ce970.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 900</span>
+           <h2 className='font-semibold text-2xl text-center '>Butterfly Pendant</h2>
+          </div>
+      </figure>
+      
+      </div>
+    </>
+  )
+}
