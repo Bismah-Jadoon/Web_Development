7e0a0facfa439910f613aeb9499d46c411fd1696
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2025-02-20 15:23:50 +0500

    Add files via upload

---
+++ Online_Shopping_Store/src/Components/PartyWear/PartyWear.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+export default function PartyWear() {
+  return (
+    <>
+         <div className='grid grid-cols-4 gap-4'>
+      <figure className=' h-[31rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/6c/69/6a/6c696ab2a33da7055ff1aeaa4d36059a.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 8,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Purple Maxi Gown </h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/aa/39/eb/aa39eb6258d05657743b01791eef86f0.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 10,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Grey Gown</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/4a/9b/eb/4a9beb9ce9c3bd3e78c6186e07d2289d.jpg" alt="" />
+        <div className='transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 4000</span>
+           <h2 className='font-semibold text-2xl text-center '>Mustard Gown </h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 mt-24'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/64/85/92/648592d3dba025a7657b6acf530db119.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 10,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Anarkali Suit </h2>
+          </div>
+      </figure> 
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-12'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/53/9d/2b/539d2b55bcc2bfcb7876f042fb0e71d4.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 19,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Peach Gown</h2>
+          </div>
+      </figure>
+      
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-12'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/d5/a1/a9/d5a1a9bbb5969673d03486bda5a99713.jpg" alt="" />
+        <div className='transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:text-[#3b7fcb] hover:underline'>Rs 13,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Elegant Maxi</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-12'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/bd/2a/30/bd2a301689fac6b2e587142087299d0f.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 20,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Pink Gown</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12 -mt-12'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/ed/91/29/ed9129b4d864ff01a624faee5868b82e.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 13,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Blue Gown</h2>
+          </div>
+      </figure>
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/736x/c4/00/7e/c4007e454e0c5858f878113df2c670ad.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 13,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Peach Maxi</h2>
+          </div>
+      </figure> 
+      <figure className=' h-[32rem] w-[20rem] rounded-xl m-12'>
+        <img className='p-6 cursor-pointer' src="https://i.pinimg.com/564x/a4/e4/11/a4e411d7e3e5c1ad961dbe1d192c7af9.jpg" alt="" />
+        <div className=' transition delay-150 ease-in-out hover:scale:110 cursor-pointer'>
+            <span className='ml-[8rem] text-xl font-semibold  hover:underline hover:text-[#3b7fcb]'>Rs 12,000</span>
+           <h2 className='font-semibold text-2xl text-center '>Mustard Gown</h2>
+          </div>
+      </figure>
+      
+    </div>
+    </>
+  )
+}
